7dc69a6bd1c47f67c5d7cba56d7b8a6b69129a45
camilosaenz <rubiansaenz1997@gmail.com>
2021-01-28 08:34:34 -0500

    Parte 1

--- parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -1,13 +1,15 @@
 package edu.eci.arsw.primefinder;
 
 public class Main {
-
 	public static void main(String[] args) {
 		
-		PrimeFinderThread pft=new PrimeFinderThread(0, 30000000);
+		PrimeFinderThread uno=new PrimeFinderThread(0, 10000000);
+		PrimeFinderThread dos=new PrimeFinderThread(10000001, 20000000);
+		PrimeFinderThread tres=new PrimeFinderThread(20000001, 30000000);
 		
-		pft.start();
-		
+		uno.start();
+		dos.start();
+		tres.start();
 		
 	}
 	
